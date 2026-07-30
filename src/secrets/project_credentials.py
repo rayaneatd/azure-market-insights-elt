3b59ed5b820c9dbf718e931be9db62f805a780fb
rayaneatd <179324378+rayaneatd@users.noreply.github.com>
2026-07-30 14:43:26 +0100

    style (credentials): made the script better looking

--- src/secrets/project_credentials.py
+++ src/secrets/project_credentials.py
@@ -4,24 +4,20 @@ import os
 load_dotenv()
 
 # ==================================================================
-# *********** DEV CREDENTIALS **************************************
+# *********** CREDENTIALS ******************************************
 # ==================================================================
 
     # azure storage credentials
 dev_STORAGE_ACCOUNT_URL       = os.getenv("dev_STORAGE_ACCOUNT_URL")
 dev_STORAGE_CONNECTION_STRING = os.getenv("dev_STORAGE_CONNECTION_STRING")
 
+prod_STORAGE_ACCOUNT_URL      = os.getenv("prod_STORAGE_ACCOUNT_URL")
+
+
     # twitch credentials
 TWITCH_CLIENT_ID              = os.getenv("TWITCH_CLIENT_ID")
 TWITCH_CLIENT_SECRET          = os.getenv("TWITCH_CLIENT_SECRET")
 
-
-# ==================================================================
-# *********** PROD CREDENTIALS *************************************
-# ==================================================================
-
-    # azure storage credentials
-prod_STORAGE_ACCOUNT_URL = os.getenv("prod_STORAGE_ACCOUNT_URL")
 
 
 # ==================================================================
